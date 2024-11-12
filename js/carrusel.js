ecdeed0c9391877544e52aa778723e2687e5b81f
franortiz01 <fortiz156@alumno.unlam.edu.ar>
2024-11-12 18:20:16 -0300

    agrego indicadores de imagenes al carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,9 +1,11 @@
 const imagenes = document.querySelectorAll('.section-slider__imagen');
+const indicadores = document.querySelectorAll('.indicador');
 let indiceActual = 0;
 const tiempoIntervalo = 3000;
 
 function mostrarImagen() {
     imagenes.forEach((imagen, i) => imagen.classList.toggle('active', i === indiceActual));
+    indicadores.forEach((indicador, i) => indicador.classList.toggle('active', i === indiceActual));
 }
 
 function cambiarImagen(paso) {
@@ -20,6 +22,14 @@ document.querySelector('.next-button').addEventListener('click', (e) => {
 document.querySelector('.prev-button').addEventListener('click', (e) => {
     e.preventDefault();
     cambiarImagen(-1);
+});
+
+indicadores.forEach((indicador) => {
+    indicador.addEventListener('click', (e) => {
+        indiceActual = parseInt(indicador.getAttribute('data-index'));
+        mostrarImagen();
+        reiniciarIntervalo();
+    });
 });
 
 let intervalo = setInterval(() => cambiarImagen(1), tiempoIntervalo);
